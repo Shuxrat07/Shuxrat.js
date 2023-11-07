f0512766129cbc79ccac3b8d820a7f46c361eb35
Shuxrat07 <150110204+Shuxrat07@users.noreply.github.com>
2023-11-07 10:52:44 +0500

    Create misol.js
    misol fayl qoʻshildi

---
+++ misol.js
@@ -0,0 +1,33 @@
+//2-misol
+let a=12;
+if(a>0){
+  a+=1
+} else{
+  a-=2
+}
+console.log("Natija",a)
+
+//4misol
+let a1=6;
+let b1=-7;
+let c=-9;
+let man=0;
+if (a1>0){
+  man=man+1
+}else{
+  mus=mus+1
+}
+console.log(man)
+//6 4misol
+let a=parseInt("butun son kiriting:",);
+let b=parseInt("butun son kiriting:",)
+
+a=parseInt(a)
+b=parseInt(b)
+
+if (a>b){
+  console.log("katta son:",a);
+}else{
+  console.log("katta son:",b);
+}
+
